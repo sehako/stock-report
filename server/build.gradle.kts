b9eef20f99f674b82ebd44c4d4ba00d777865b78
sehako <dhtpgkr1999@gmail.com>
2026-07-02 14:44:27 +0900

    test(server): 테스트 데이터베이스를 PostgreSQL 컨테이너로 전환

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -32,8 +32,10 @@ dependencies {
     runtimeOnly("org.flywaydb:flyway-database-postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("com.h2database:h2")
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
+    testImplementation("org.testcontainers:testcontainers-postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
